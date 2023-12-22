6c7c364d9251a703b4c65bf4bbec734495208ed8
Leeco <lee.coppens@hotmail.be>
2023-12-22 15:26:37 +0100

    small user change

--- src/fourcolors/menu/Users.java
+++ src/fourcolors/menu/Users.java
@@ -6,14 +6,25 @@ import java.util.Map;
 
 public class Users {
     private final Map<String, User> users;
+    private final static String FILE_LOCATION = "users-save-data.cvs";
 
     public Users() {
         users = new HashMap<>();
+        load(FILE_LOCATION);
     }
 
     public void addUser(User user) {
         if (!users.containsKey(user.getName())) {
             users.put(user.getName(), user);
+            save(FILE_LOCATION);
         }
+    }
+
+    public void save(String filename) {
+
+    }
+
+    public void load(String filename) {
+
     }
 }
