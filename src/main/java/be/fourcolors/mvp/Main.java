fb9245af878b7ced1616f11dbc9d3d6e69a68143
Leeco <lee.coppens@hotmail.be>
2024-02-09 16:46:18 +0100

    Scene

--- src/main/java/be/fourcolors/mvp/Main.java
+++ src/main/java/be/fourcolors/mvp/Main.java
@@ -3,11 +3,15 @@ package be.fourcolors.mvp;
 import be.fourcolors.mvp.view.login.LoginView;
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+    private final static String ICON_PATH = "/images/icon.png";
     public void start(Stage primaryStage){
         LoginView loginView = new LoginView();
+        Image sceneIcon = new Image(ICON_PATH);
+        primaryStage.getIcons().add(sceneIcon);
         primaryStage.setScene(new Scene(loginView));
         primaryStage.setTitle("Four Colors");
         primaryStage.setMinHeight(600);
