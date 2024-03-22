ce862a32ede02b6fee86d554e01131dc0e03d096
Leeco <lee.coppens@hotmail.be>
2024-03-22 01:09:22 +0100

    Fixed loading screen

--- src/main/java/be/fourcolors/mvp/view/splash/SplashView.java
+++ src/main/java/be/fourcolors/mvp/view/splash/SplashView.java
@@ -2,7 +2,6 @@ package be.fourcolors.mvp.view.splash;
 
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
@@ -11,7 +10,6 @@ import javafx.scene.layout.VBox;
 import java.util.Objects;
 
 public class SplashView extends BorderPane {
-    private Label laText;
     private ImageView ivLogo;
     private ImageView ivLoading;
     private VBox vBox;
@@ -28,8 +26,6 @@ public class SplashView extends BorderPane {
         Image loading = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/images/loading.gif")));
         ivLoading = new ImageView(loading);
 
-        laText = new Label("Loading...");
-
         vBox = new VBox();
     }
 
@@ -39,11 +35,10 @@ public class SplashView extends BorderPane {
         ivLoading.setSmooth(true);
 
         ivLogo.setPreserveRatio(true);
-        ivLogo.setFitWidth(600);
         ivLogo.setFitHeight(450);
         ivLogo.setSmooth(true);
 
-        vBox.setSpacing(10);
+        VBox.setMargin(ivLoading, new Insets(-75, 0, 0, 0));
         vBox.setAlignment(Pos.CENTER);
         BorderPane.setMargin(vBox, new Insets(50));
 
